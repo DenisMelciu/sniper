aee0a62f504abb90cd8b7273a4df509a48195951
DenisMelciu <71292312+DenisMelciu@users.noreply.github.com>
2020-09-24 19:24:57 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+function nova () {
+    led.plotBrightness(2, 0, 255)
+    led.plotBrightness(2, 1, 255)
+    led.plotBrightness(2, 2, 255)
+    led.plotBrightness(2, 3, 255)
+    led.plotBrightness(2, 4, 255)
+    led.plotBrightness(0, 2, 255)
+    led.plotBrightness(1, 2, 255)
+    led.plotBrightness(3, 2, 255)
+    led.plotBrightness(4, 2, 255)
+}
+let snipe = 5
+let broj = 5
+basic.forever(function () {
+	
+})
